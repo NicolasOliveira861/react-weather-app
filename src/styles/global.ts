51b5f43bcb202e50308cf0642dc1d1035434d6dd
Nicolas Oliveira <nicksouza861@gmail.com>
2022-08-24 22:26:35 -0300

    feat: project configuration

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -5,8 +5,7 @@ const GlobalStyles = globalCss({
     margin: 0,
     padding: 0,
     boxSizing: 'border-box',
-    fontFamily:
-      '-apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen-Sans, Ubuntu, Cantarell, "Fira Sans", "Droid Sans", "Helvetica Neue", Helvetica, "ヒラギノ角ゴ Pro W3", "Hiragino Kaku Gothic Pro", メイリオ, Meiryo, "ＭＳ Ｐゴシック", Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"',
+    fontFamily: '',
     fontSize: '16px',
     lineHeight: 'normal',
     letterSpacing: 'normal',
